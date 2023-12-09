41f50b69e4672ebfaec07a1d2d899b1c846a4db1
AlexGameDev <alexgamedev2000@gmail.com>
2023-12-09 12:42:32 +0100

    FEAT - Add random generation utilities

---
+++ BSDesigner/BSDesigner.Core/Utils/DefaultRandom.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BSDesigner.Core.Utils
+{
+    public class DefaultRandom : IRandom
+    {
+        private readonly Random _random = new Random();
+        public int NextInt(int minIncludedValue, int maxExcludedValue) => _random.Next(minIncludedValue, maxExcludedValue);
+
+        public double NextDouble() => _random.NextDouble();
+    }
+}
---
+++ BSDesigner/BSDesigner.Core/Utils/IRandom.cs
@@ -0,0 +1,19 @@
+﻿namespace BSDesigner.Core.Utils
+{
+    public interface IRandom
+    {
+        /// <summary>
+        /// Generate a value between <see cref="minIncludedValue"/> and <paramref name="maxExcludedValue"/>.
+        /// </summary>
+        /// <param name="minIncludedValue">The minimum inclusive value.</param>
+        /// <param name="maxExcludedValue">The maximum exclusive value.</param>
+        /// <returns>The generated value.</returns>
+        public int NextInt(int minIncludedValue, int maxExcludedValue);
+
+        /// <summary>
+        /// Generate a double value greater or equal than 0.0 and lower than 1.0.
+        /// </summary>
+        /// <returns>The generated value</returns>
+        public double NextDouble();
+    }
+}
