16c4dda2c388a55284d169facf0827acf2af9c70
AlexGameDev <alexgamedev2000@gmail.com>
2023-12-27 23:06:24 +0100

    FEAT - Import dlls and create BsRunner

---
+++ BSDesigner.UnityTool/Assets/Scripts/Runtime/BSRunner.cs
@@ -0,0 +1,132 @@
+using UnityEngine;
+using BSDesigner.Core;
+
+namespace BSDesigner.UnityTool.Runtime
+{
+    /// <summary>
+    /// Base class for all behaviour system runners.
+    /// </summary>
+    public abstract class BSRunner : MonoBehaviour
+    {
+        [Tooltip("Restart execution when finished?")]
+        public bool ExecuteOnLoop;
+
+        bool _systemRunning;
+
+        BehaviourEngine _executionEngine;
+
+        /// <summary>
+        /// Override this method to set the context that the graph will use.
+        /// </summary>
+        /// <returns>The context created.</returns>
+        protected virtual UnityExecutionContext CreateContext()
+        {
+            var context = new UnityExecutionContext(this);
+            return context;
+        }
+
+        /// <summary>
+        /// Gets the main engine that will be executed.
+        /// </summary>
+        /// <returns>The execution <see cref="BehaviourEngine"></see></returns>
+        protected abstract BehaviourEngine CreateBehaviourSystem();
+
+
+        private void Awake() => OnAwake();
+
+        private void Start() => OnStarted();
+
+        private void Update() => OnUpdated();
+
+        private void OnEnable() => OnEnabled();
+
+        private void OnDisable() => OnDisabled();
+
+
+        /// <summary>
+        /// Called in awake event.
+        /// Create the behaviour graph and set the context.
+        /// </summary>
+        protected virtual void OnAwake()
+        {
+            _executionEngine = CreateBehaviourSystem();
+
+            if (_executionEngine != null)
+            {
+                UnityExecutionContext context = CreateContext();
+                _executionEngine.SetContext(context);
+            }
+        }
+
+        /// <summary>
+        /// Called in start event.
+        /// Starts the graph execution.
+        /// </summary>
+        protected virtual void OnStarted()
+        {
+            if (_executionEngine != null)
+            {
+                _executionEngine.Start();
+                _systemRunning = true;
+            }
+            else
+            {
+                Debug.LogWarning("EXECUTION ERROR: This runner has not graph attached.", this);
+                Destroy(this);
+            }
+        }
+
+        /// <summary>
+        /// Called in update event.
+        /// Update the graph executions and restart it when finish if ExecuteOnLoop flag is raised.
+        /// </summary>
+        protected virtual void OnUpdated()
+        {
+            if (_executionEngine != null)
+            {
+                if (_executionEngine.Status != Status.Running) return;
+
+                _executionEngine.Update();
+
+                if (_executionEngine.Status == Status.Running) return;
+
+                _executionEngine.Stop();
+
+                if (ExecuteOnLoop) _executionEngine.Start();
+
+            }
+            else
+            {
+                Debug.LogWarning("EXECUTION ERROR: This runner has not graphs attached.", this);
+                Destroy(this);
+            }
+        }
+
+        /// <summary>
+        /// Called in ondisable event.
+        /// The method called in this event depends on dontStopOnDisable configuration.
+        /// </summary>
+        protected virtual void OnDisabled()
+        {
+            if (!_systemRunning || _executionEngine == null)
+                return;
+
+            _executionEngine.Stop();
+        }
+
+        /// <summary>
+        /// Called in onenable event.
+        /// The method called in this event depends on dontStopOnDisable configuration.
+        /// </summary>
+        protected virtual void OnEnabled()
+        {
+            if (!_systemRunning || _executionEngine == null)
+                return;
+
+            if (_executionEngine.Status == Status.None)
+            {
+                _executionEngine.Start();
+            }
+        }
+    }
+}
---
+++ BSDesigner.UnityTool/Assets/Scripts/Runtime/UnityExecutionContext.cs
@@ -0,0 +1,35 @@
+﻿using BSDesigner.Core;
+using UnityEngine;
+
+namespace BSDesigner.UnityTool.Runtime
+{
+    public class UnityExecutionContext : ExecutionContext
+    {
+        /// <summary>
+        /// The script that executes the graph with this context.
+        /// </summary>
+        public Component RunnerComponent => _runnerComponent;
+
+        /// <summary>
+        /// The game object of the agent.
+        /// </summary>
+        public GameObject GameObject => _runnerComponent.gameObject;
+
+        /// <summary>
+        /// The transform of the agent.
+        /// </summary>
+        public Transform Transform => GameObject.transform;
+
+        private readonly Component _runnerComponent;
+
+        /// <summary>
+        /// Create a new unity execution context with a runner script component. Use this constructor
+        /// to access methods in the runner component with custom actions or perceptions.
+        /// </summary>
+        /// <param name="runnerComponent">The runner component.</param>
+        public UnityExecutionContext(Component runnerComponent)
+        {
+            _runnerComponent = runnerComponent;
+        }
+    }
+}
