f5d257b9dcb6b94071bea7c7e9fbe14edfdd1417
AlexGameDev <alexgamedev2000@gmail.com>
2023-12-31 14:40:45 +0100

    FEAT - Add reference converter to json serialization project

---
+++ BSDesigner/BehaviourDesigner.JsonSerialization/Converters/ReferenceConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace BSDesigner.JsonSerialization
+{
+    /// <summary>
+    /// Custom converter that serializes all instances of <typeparamref name="T"/>
+    /// </summary>
+    /// <typeparam name="T">The type of the references.</typeparam>
+    public class ReferenceConverter<T> : JsonConverter<T> where T : class
+    {
+        public List<T> referencedObjects = new List<T>();
+
+        public override void WriteJson(JsonWriter writer, T? value, JsonSerializer serializer)
+        {
+            if (value != null)
+            {
+                writer.WriteValue(referencedObjects.Count);
+                referencedObjects.Add(value);
+            }
+            else
+            {
+                writer.WriteNull();
+            }
+        }
+
+        public override T? ReadJson(JsonReader reader, Type objectType, T? existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Integer)
+            {
+                var referenceIndex = reader.ReadAsInt32() ?? -1;
+                if (referenceIndex < 0 || referenceIndex >= referencedObjects.Count) return null;
+                return referencedObjects[referenceIndex];
+            }
+            return null;
+        }
+    }
+}
---
+++ BSDesigner/BehaviourDesigner.JsonSerialization/JsonSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace BSDesigner.JsonSerialization
+{
+    public class JsonSettings
+    {
+        public IReadOnlyList<JsonConverter> Converters => converters;
+        public bool Pretty { get; set; }
+
+        private List<JsonConverter> converters = new List<JsonConverter>();
+
+        public void AddReferenceConverter<T>(ref List<T> references) where T : class
+        {
+            var converter = new ReferenceConverter<T>();
+            converter.referencedObjects = references;
+        }
+    }
+}
