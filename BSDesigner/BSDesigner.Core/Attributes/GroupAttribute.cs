da63d2c2873c4ba706a03c572d09846099a3d3e6
AlexGameDev <alexgamedev2000@gmail.com>
2024-03-03 12:51:46 +0100

    Add metadata attributes for bs elements

---
+++ BSDesigner/BSDesigner.Core/Attributes/GroupAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BSDesigner.Core.Attributes
+{
+    /// <summary>
+    /// Specify a group for an element
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class GroupAttribute
+    {
+        /// <summary>
+        /// Name of the group
+        /// </summary>
+        public string Name { get; set; }
+
+        public GroupAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ BSDesigner/BSDesigner.Core/Attributes/SummaryAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BSDesigner.Core.Attributes
+{
+    /// <summary>
+    /// Specify a summary description of an element
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class SummaryAttribute : Attribute
+    {
+        /// <summary>
+        /// Text content
+        /// </summary>
+        public string Text { get; set; }
+
+        public SummaryAttribute(string text)
+        {
+            Text = text;
+        }
+    }
+}
