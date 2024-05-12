4ad11ce0f1c1aa4c4188c92cbd2cdbce8df3c96f
AlexGameDev <alexgamedev2000@gmail.com>
2024-05-12 11:07:15 +0200

    Add Visual tool data model

---
+++ Unity/Assets/VisualTool/Runtime/BSData.cs
@@ -0,0 +1,44 @@
+﻿using BSDesigner.Core;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BSDesigner.Unity.VisualTool
+{
+    /// <summary>
+    /// Data class that stores a serialized behaviour system.
+    /// </summary>
+    public class BSData : ISerializationCallbackReceiver
+    {
+        /// <summary>
+        /// The list of unity object referenced in the system as parameters.
+        /// </summary>
+        [SerializeField] private List<Object> referencedObjects;
+
+        /// <summary>
+        /// The list of behaviour engines in the system
+        /// </summary>
+        public List<BehaviourEngine> Engines => _engines;
+
+        List<BehaviourEngine> _engines = new List<BehaviourEngine>();
+        bool m_DirtyFlag;
+
+        /// <summary>
+        /// Enable the serialization after a change.
+        /// </summary>
+        public void SetDirty() => m_DirtyFlag = true;
+
+        public void OnAfterDeserialize()
+        {
+            //TODO: Deserialize data
+        }
+
+        public void OnBeforeSerialize()
+        {
+            if (!m_DirtyFlag) return;
+
+            //TODO: Serialize data
+
+            m_DirtyFlag = false;
+        }
+    }
+}
---
+++ Unity/Assets/VisualTool/Runtime/BSVisualPrototype.cs
@@ -0,0 +1,36 @@
+﻿using BSDesigner.Core;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace BSDesigner.Unity.VisualTool.Runtime.Assets.VisualTool.Runtime
+{
+    /// <summary>
+    /// Behaviour system prototype that can be created using the visual tool.
+    /// </summary>
+    [CreateAssetMenu(menuName = "BSDesigner/VisualTool/Prototype", fileName = "NewBSPrototype")]
+    public class BSVisualPrototype : BSPrototype
+    {
+        [SerializeField] private BSData data;
+
+        /// <summary>
+        /// The behaviour system data of the asset (for the editor)
+        /// </summary>
+        public BSData Data => data;
+
+        public override BehaviourEngine CreateBehaviourSystem()
+        {
+            var engines = data.Engines;
+            ModifySystem(engines);
+            return engines.FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Override this method to modify the system created in the editor.
+        /// </summary>
+        /// <param name="engines">The list of engines in the system.</param>
+        protected virtual void ModifySystem(IReadOnlyList<BehaviourEngine> engines)
+        {
+        }
+    }
+}
