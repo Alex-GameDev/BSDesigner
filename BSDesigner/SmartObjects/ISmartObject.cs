1ba9879410f99b63c5a891d7f5f272e99f3ad59e
AlexGameDev <alexgamedev2000@gmail.com>
2024-05-12 19:36:07 +0200

    Smart object api base

---
+++ BSDesigner/SmartObjects/ISmartObject.cs
@@ -0,0 +1,13 @@
+﻿using BSDesigner.Core;
+
+namespace BSDesigner.SmartObjects
+{
+    public interface ISmartObject
+    {
+        /// <summary>
+        /// Request the interaction.
+        /// </summary>
+        /// <returns> The interaction provided. </returns>
+        ActionTask RequestInteraction(ExecutionContext context);
+    }
+}
---
+++ BSDesigner/SmartObjects/RequestActionTask.cs
@@ -0,0 +1,67 @@
+﻿using BSDesigner.Core;
+
+namespace BSDesigner.SmartObjects
+{
+    /// <summary>
+    /// Action that request a behaviour to a smart object. 
+    /// </summary>
+    public abstract class RequestActionTask : ActionTask
+    {
+        /// <summary> 
+        /// The current interaction that this action is executing. 
+        /// </summary>
+        ActionTask? currentInteraction;
+
+        /// <summary>
+        /// The context that the request action will propagate throught the provided actions.
+        /// </summary>
+        protected ExecutionContext Context { get; private set; } = null!;
+
+        /// <summary>
+        /// Get the provider that this request action will use to find the smart objects.
+        /// </summary>
+        /// <returns>The smart object provider.</returns>
+        protected abstract ISmartObject GetSmartObject();
+
+        public override void SetContext(ExecutionContext context)
+        {
+           Context = context;
+        }
+
+        protected override void OnBeginTask()
+        {
+            var smartObject = GetSmartObject();
+
+            if(smartObject != null)
+            {
+                var interaction = smartObject.RequestInteraction(Context);
+                this.currentInteraction = interaction;
+                interaction.Start();
+            }
+        }
+
+        protected override void OnEndTask()
+        {
+            currentInteraction?.Stop();
+            currentInteraction = null;
+        }
+
+        protected override void OnPauseTask()
+        {
+            currentInteraction?.Pause();
+        }
+
+        protected override Status OnUpdateTask()
+        {
+            if (currentInteraction != null)
+            {
+                var status = currentInteraction.Update();
+                return status;
+            }
+            else
+            {
+                return Status.Failure;
+            }
+        }
+    }
+}
