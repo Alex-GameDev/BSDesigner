e0a92d3e0b9de00d9eb94924d93b1e74a90c8fee
AlexGameDev <alexgamedev2000@gmail.com>
2024-05-18 13:25:35 +0200

    Editor window interfaces

---
+++ Unity/Assets/VisualTool/Editor/IEngineListView.cs
@@ -0,0 +1,16 @@
+﻿using BSDesigner.Core;
+using System;
+
+namespace BSDesigner.Unity.VisualTool.Editor
+{
+    public interface IEngineListView
+    {
+        public event Action DataChanged;
+
+        public event Action<BehaviourEngine> EngineSelected;
+
+        public void Update(BehaviourEngine engine);
+
+        public void Clear();
+    }
+}
---
+++ Unity/Assets/VisualTool/Editor/IEngineView.cs
@@ -0,0 +1,14 @@
+﻿using BSDesigner.Core;
+using System;
+
+namespace BSDesigner.Unity.VisualTool.Editor
+{
+    public interface IEngineView
+    {
+        public event Action DataChanged;
+
+        public void Update(BehaviourEngine engine);
+
+        public void Clear();
+    }
+}
