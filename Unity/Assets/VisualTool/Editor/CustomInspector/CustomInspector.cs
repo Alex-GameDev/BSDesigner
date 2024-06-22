9d9766abff1d5599a8b7d319c756aad6a22d8e0c
AlexGameDev <alexgamedev2000@gmail.com>
2024-06-22 20:15:33 +0200

    Render object subfields

--- Unity/Assets/VisualTool/Editor/CustomInspector/CustomInspector.cs
+++ Unity/Assets/VisualTool/Editor/CustomInspector/CustomInspector.cs
@@ -1,8 +1,11 @@
 using BSDesigner.Core;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace BSDesigner.Unity.VisualTool.Editor.Inspector
@@ -29,7 +32,7 @@ namespace BSDesigner.Unity.VisualTool.Editor.Inspector
 
             if(obj != null)
             {
-                var typeFields = obj.GetType().GetFields(BindingFlags.Instance | BindingFlags.Public);
+                var typeFields = obj.GetType().GetFields(BindingFlags.Instance | BindingFlags.Public).OrderBy(field => field.MetadataToken);
                 foreach (var field in typeFields)
                 {
                     fields.Add(CreateReflectedField(field, obj));
@@ -53,6 +56,8 @@ namespace BSDesigner.Unity.VisualTool.Editor.Inspector
             return new ReflectedField(fieldInfo, parentObj);
         }
     }
+    
+
 
     public class ReflectedField
     {
@@ -60,11 +65,21 @@ namespace BSDesigner.Unity.VisualTool.Editor.Inspector
         private object parentObj;
         private object cachedValue;
 
+        private List<ReflectedField> subFields = new List<ReflectedField>();
+
         public ReflectedField(FieldInfo fieldInfo, object parentObj)
         {
             this.fieldInfo = fieldInfo;
             this.parentObj = parentObj;
             this.cachedValue = fieldInfo.GetValue(parentObj);
+            if(cachedValue != null)
+            {
+                var typeFields = cachedValue.GetType().GetFields(BindingFlags.Instance | BindingFlags.Public).OrderBy(field => field.MetadataToken);
+                foreach (var field in typeFields)
+                {
+                    subFields.Add(CreateReflectedField(field, cachedValue));
+                }
+            }
         }
 
         public void Render()
@@ -90,12 +105,77 @@ namespace BSDesigner.Unity.VisualTool.Editor.Inspector
             {
                 newValue = EditorGUILayout.EnumPopup(this.fieldInfo.Name, (Enum)cachedValue);
             }
+            else if(!fieldInfo.FieldType.IsValueType)
+            {
+                if(fieldInfo.FieldType.IsAbstract)
+                {
+                    if(fieldInfo.GetValue(this.parentObj) != null)
+                    {
+                        EditorGUILayout.BeginHorizontal();
+                        EditorGUILayout.LabelField(this.fieldInfo.Name, this.fieldInfo.GetValue(parentObj).GetType().Name);
+                        if (GUILayout.Button("x"))
+                        {
+                            this.ResetValue();
+                            EditorGUILayout.EndHorizontal();
+                            return;
+                        }
+                        EditorGUILayout.EndHorizontal();
+
+                        foreach (var item in this.subFields)
+                        {
+                            item.Render();
+                        }
+                    }
+                    else
+                    {
+                        EditorGUILayout.BeginHorizontal();
+                        EditorGUILayout.LabelField(this.fieldInfo.Name);
+                        if (GUILayout.Button("Set"))
+                        {
+                            this.OpenTypeSelector();
+                        }
+                        EditorGUILayout.EndHorizontal();
+                    }
+                }
+
+            }
 
             if (newValue != cachedValue)
             {
                 this.cachedValue = newValue;
                 this.UpdateValue();
             }
+        }
+
+        private void ResetValue()
+        {
+            this.cachedValue = null;
+            this.UpdateValue();
+            this.subFields.Clear();
+        }
+
+        private void OpenTypeSelector()
+        {
+            SearchWindow.Open(new SearchWindowContext(Vector2.zero), SearchWindowProvider.Create(this.fieldInfo.FieldType, this.OnSetType));
+        }
+
+        private void OnSetType(Type type)
+        {
+            Debug.Log("Set value");
+            var value = Activator.CreateInstance(type);
+            this.cachedValue = value;
+            this.UpdateValue();
+            var typeFields = value.GetType().GetFields(BindingFlags.Instance | BindingFlags.Public).OrderBy(field => field.MetadataToken);
+            foreach (var field in typeFields)
+            {
+                subFields.Add(CreateReflectedField(field, value));
+            }
+        }
+
+        private ReflectedField CreateReflectedField(FieldInfo fieldInfo, object parentObj)
+        {
+            Type type = fieldInfo.FieldType;
+            return new ReflectedField(fieldInfo, parentObj);
         }
 
         private void UpdateValue()
